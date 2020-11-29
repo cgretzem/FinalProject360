dca0d22638ce679ad481a044d3fe0e128a08e78c
maste <maste@DESKTOP-6215NHE>
2020-11-28 16:47:52 -0800

    Added bones to student class and completed methods

--- CSE360FinalProject/src/finalProject/Student.java
+++ CSE360FinalProject/src/finalProject/Student.java
@@ -1,5 +1,66 @@
 package finalProject;
 
-public class Student {
+import java.util.HashMap;
 
+public class Student 
+{
+	private int id;
+	private String firstName, lastName, program, level, asurite;
+	private HashMap<String, Integer> dateMap;
+	
+	public Student(int id, String firstName, String lastName, String program, String level, String asurite)
+	{
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.program = program;
+		this.level = level;
+		this.asurite = asurite;
+		dateMap = new HashMap<String, Integer>();
+	}
+	
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	
+	
+	public String getLastName()
+	{
+		return lastName;
+	}
+	
+	public int getID()
+	{
+		return id;
+	}
+	
+	public String getProg()
+	{
+		return program;
+	}
+	
+	public String getASU()
+	{
+		return asurite;
+	}
+	
+	public String getLevel()
+	{
+		return level;
+	}
+	
+
+	public HashMap<String, Integer> getDates()
+	{
+		return dateMap;
+	}
+	
+	public void addDate(String date, int time)
+	{
+		Integer newTime = (Integer)time;
+		dateMap.put(date, newTime);
+		
+	}
 }
+
