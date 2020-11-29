eacffab45c885f036b1b7359675cd6dcc6b83ee6
maste <maste@DESKTOP-6215NHE>
2020-11-28 17:53:44 -0800

    Added openAttendence and openRoster as well as student implementation to Roster

--- CSE360FinalProject/src/finalProject/Repository.java
+++ CSE360FinalProject/src/finalProject/Repository.java
@@ -1,5 +1,112 @@
 package finalProject;
 
-public class Repository {
+import java.awt.Component;
+
+import java.io.File;
+import java.util.Stack;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+
+public class Repository
+{
+	private Stack<Student> studentList;
+	private Stack<String> dateList;
+	
+	public Repository()
+	{
+		studentList = new Stack<Student>();
+		dateList = new Stack<String>();
+	}
+	
+	public void openAttendence(String date, Component parent) throws FileNotFoundException, IOException
+	{
+		dateList.add(date);
+		File newFile = loadFile(parent);
+		if(newFile == null)
+		{
+			throw new FileNotFoundException("Wrong type of file");
+		}
+		BufferedReader br = null;
+		String line = "";
+		String delimiter = ",";
+		
+		br = new BufferedReader(new FileReader(newFile));
+		while((line = br.readLine()) != null)
+		{
+			String[] student = line.split(delimiter);
+			if (student.length != 2)
+			{
+				br.close();
+				throw new RuntimeException("File was not formatted correctly");
+			}
+			else
+			{
+				for(int i = 0; i < studentList.size(); i++)
+				{
+					if(studentList.get(i).getASU().equals(student[0]))
+					{
+						studentList.get(i).addDate(date, Integer.valueOf(student[1]));
+					}
+				}
+			}
+			
+		}
+		br.close();
+	}
+	
+	
+	public File loadFile(Component parent)
+	{
+		//selector sets newFile = user chosen CSV file, otherwise newFile = null;
+			File newFile = null;
+			JFileChooser chooser = new JFileChooser();
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV files", ".csv"); 
+			chooser.setFileFilter(filter);
+			int returnVal = chooser.showOpenDialog(parent);
+			if(returnVal == JFileChooser.APPROVE_OPTION)
+			{
+				newFile = chooser.getSelectedFile();
+			}
+			
+			return newFile;
+	}
+	
+	public void openRoster(Component parent) throws IOException, FileNotFoundException
+	{
+		File newFile = loadFile(parent);
+		if(newFile == null)
+		{
+			throw new FileNotFoundException("Wrong type of file");
+		}
+		BufferedReader br = null;
+		String line = "";
+		String delimiter = ",";
+		
+		br = new BufferedReader(new FileReader(newFile));
+		while((line = br.readLine()) != null)
+		{
+			String[] student = line.split(delimiter);
+			if (student.length != 6)
+			{
+				br.close();
+				throw new RuntimeException("File was not formatted correctly");
+			}
+			else
+			{
+				studentList.add(new Student(Integer.valueOf(student[0]),student[1],student[2],student[3],student[4],student[5]));
+			}
+			
+		}
+		br.close();
+			
+		
+		
+	}
+	
+	//public Stack getPoints(){} TODO
 
 }
